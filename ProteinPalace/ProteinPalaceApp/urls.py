7132bfa3134e5469b5b2be90e0e8804b0c5cc660
Frost1801 <sergio.cibecchini@gmail.com>
2023-05-26 12:20:35 +0200

    created browse page

--- ProteinPalace/ProteinPalaceApp/urls.py
+++ ProteinPalace/ProteinPalaceApp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views 
 urlpatterns = [
 	path('', views.mainPage, name = 'mainPage'), 
-    
+    path('browse/', views.browse, name = 'browse'),
+    path('myrecipes/', views.myrecipes, name = 'myrecipes'),
+    path('create/', views.create, name = 'create'),
+    path('favorites/', views.favorites, name = 'favorites'),
+    path('myaccount/', views.myaccount, name = 'myaccount'),
 ]
--- ProteinPalace/ProteinPalaceApp/views.py
+++ ProteinPalace/ProteinPalaceApp/views.py
@@ -3,4 +3,25 @@ from django.http import HttpResponse
 
 # Create your views here.
 def mainPage(request):
+    myDict = {
+        "title": "Home",
+        "current_page": "home",
+    }
     return render(request,'index.html')
+
+def browse(request):
+    myDict = {
+        "title": "Browse",
+        "current_page": "browse",
+    }
+    return render(request,'browse.html', context=myDict)
+
+def myrecipes(request):
+    return HttpResponse("My Recipes")
+
+def create(request):
+    return HttpResponse("Create")
+def favorites(request):
+    return HttpResponse("Favorites")
+def myaccount(request):
+    return HttpResponse("My Account")
