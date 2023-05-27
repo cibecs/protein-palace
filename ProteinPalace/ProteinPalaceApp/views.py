d6050a0e34169b93cb2c034b027cf69e99f7f106
Frost1801 <sergio.cibecchini@gmail.com>
2023-05-27 16:55:23 +0200

    Pagination kinda works

--- ProteinPalace/ProteinPalaceApp/views.py
+++ ProteinPalace/ProteinPalaceApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Recipe
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
@@ -10,11 +12,27 @@ def home(request):
     return render(request,'index.html', context=myDict)
 
 def browse(request):
+    num = int(request.GET.get('num', 1))  # 1 is the default value
+    max_recipes_per_page = 3  # Set the maximum number of recipes to display per page
+
+    recipes = Recipe.objects.all()
+    paginator = Paginator(recipes, max_recipes_per_page)
+
+    page = paginator.get_page(num)
+    page_range = paginator.page_range
+    max_pages = paginator.num_pages
+
     myDict = {
         "title": "Browse",
         "current_page": "browse",
+        "num": num,
+        "recipes": page,
+        "max_recipes_per_page": max_recipes_per_page,
+        "page_range": page_range,
+        "max_pages": max_pages,
     }
-    return render(request,'browse.html', context=myDict)
+    return render(request, 'browse.html', context=myDict)
+
 
 def myrecipes(request):
     return HttpResponse("My Recipes")
